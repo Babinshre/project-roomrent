339491f10b6e20ef6c2a23f77bf9185566936ca5
babin <babinshrestha5748@gmail.com>
2021-06-03 15:42:18 +0545

    changes in nabin app

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Thank you for everything its me babin shrestha</h1>
+    <h1>Thank you for everything its me nabin chauhan</h1>
 </body>
 </html>
