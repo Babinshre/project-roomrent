75dbadee3f60e3fa72a95773cb781d5c4b4cd68e
babin <babinshrestha5748@gmail.com>
2021-06-07 18:37:11 +0545

    footer edit

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -294,10 +294,10 @@
     </div>
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+      <strong>Copyright &copy; 2021-2030 <a href="#">BNBSD</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.1.0
+        <b>Version</b>1.0
       </div>
     </footer>
 
