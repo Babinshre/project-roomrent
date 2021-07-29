444e376f8f68f2045a776e2813588b72b278689d
babin <babinshrestha5748@gmail.com>
2021-07-29 15:28:00 +0545

    logo

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -45,7 +45,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 logo">
-                        <a href="/">Roomrent</a>
+                        <a href="/"><img src="{{asset('images/logoRR.png')}}" height="60px" alt=""></a>
                     </div>
                     <div class="col-md-4 right-menu">
                         <div class="row">
