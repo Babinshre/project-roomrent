b94c82470c8d1b878ba5394fcce045a8f2eaff20
babin <babinshrestha5748@gmail.com>
2021-12-24 01:14:10 +0545

    nnot-imp

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400&display=swap" rel="stylesheet">
     {{-- custom css stylesheet --}}
-    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">      
+    <link rel="stylesheet" type="text/css" href="{{asset('public/css/style.css')}}">      
     <style>
       .topPage{
         text-align: center;
